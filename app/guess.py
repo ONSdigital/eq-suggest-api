c4280b7baa7139198c02e514257f63bf1e788540
David Charles <bowavey@gmail.com>
2018-04-17 10:40:13 +0100

    Refactor Fuzzer
    - Better name.
    - Documented to better explain functionality.
    - More meaningful method names.
    - Normalise data set to remove or replace unhelpful characters.

--- app/guess.py
+++ app/guess.py
@@ -14,15 +14,26 @@ class GuessError(Exception):
     pass
 
 
+SUBSTITUTES = [
+    ('(', ''), (')', ''), (',', ' '), ('\'', ''), ('"', ''), ('\u2019', ''),
+    ('\u00e5', 'a'), ('\u00e2', 'a'), ('\u00e7', 'c'), ('\u00e9', 'e'),
+    ('\u00e8', 'e')
+]
+
+
 class Guess:
     """Guess.
 
     Guess a data item match from a simple data source using a given phrase and
     Bayesian probabilities.  The data source should be a simple json file
     containing a single list of strings.
+
+    Something about cleaned data set and charset ...
+    ...
+
     """
     MIN_N_GRAM_SIZE = 3
-    CHARSET = string.ascii_lowercase + string.digits + 'ÉéåÅÇç’-,()'
+    CHARSET = string.ascii_lowercase + string.digits + 'ÉéåÅÇç’-,()'  # Take out all, should be normalised out?
 
     def __init__(self, source):
         """Constructor.
@@ -33,7 +44,7 @@ class Guess:
         self.data = []
         self.token_to_item_name = defaultdict(list)
         self.n_gram_to_tokens = defaultdict(set)
-        self.fuzzer = Fuzzer(self.CHARSET)
+        self.pl = PhraseLookup()
 
     def init(self):
         """Initialise instance.
@@ -51,7 +62,7 @@ class Guess:
             raise GuessError(f'Failed to open data source {e}')
         except AssertionError:
             raise GuessError('Data source is not a list')
-        self.fuzzer.prime(self.data)
+        self.pl.prime(self.data)
         for item in self.data:
             # TODO Need some others "," "'". Need to cope with "é"
             norm_item = item.lower().replace("(", "").replace(")", "")
@@ -111,7 +122,7 @@ class Guess:
         return [tuple_obj[0] for tuple_obj in min_possibles]
 
     def candidates(self, query):
-        tokens = self.fuzzer.correct_phrase(query)
+        tokens = self.pl.lookup_phrase(query)
         real_tokens = self._get_real_tokens_from_possible_n_grams(tokens)
         scores = self._get_scored_items_uncollapsed(real_tokens)
         collapsed_item_to_score = self._combined_scores(scores, len(tokens))
@@ -121,58 +132,125 @@ class Guess:
         return self._filtered_results(scores)
 
 
-class Fuzzer:
-    """Fuzzer.
+class PhraseLookup:
+    """PhraseLookup.
 
-    Creates variations to check against.
+    Given a data set creates a model of known words and the probability of
+    their occurrence in a supplied the data set. When a phrase is checked
+    against the model each word in the phrase is checked against a range of
+    'edits' to establish a likely match for each word in the phrase supplied.
     """
-    def __init__(self, charset):
-        self.nwords = None
-        self.charset = charset
+    def __init__(self):
+        self.model = None
 
     def prime(self, data):
-        self.nwords = self.train(self.words(' '.join(data)))
+        """Prime the model.
 
-    def words(self, all_text):
+        Given a data set, build a normalised model that records the count of
+        each word in the data set.  When determining candidates for a lookup
+        we use the word with the highest probability of being the match.
+
+        :param (list) data: A simple list of words or phrases.
         """
+        self.model = self._build_model(self._words(data))
 
-        :param (str) all_text:
-        :returns: All matches
+    @staticmethod
+    def _words(data):
+        """Build word list.
+
+        Takes all the phrases in the data set and creates a normalised word
+        list.
+
+        :param (list) data: Input data set.
+        :returns (list): Normalised list of data set words.
         """
-        return re.findall('[a-z0-9]+', all_text.lower())
+        all_text = ' '.join(data).lower()
+        for substitute in SUBSTITUTES:
+            all_text = all_text.replace(substitute[0], substitute[1])
+        return re.findall('[a-z0-9]+', all_text)
 
-    def train(self, features):
+    @staticmethod
+    def _build_model(word_list):
+        """Build model.
+
+        Build the probabilities from a supplied word list.
+
+        :param (list) word_list: List of know words.
+        :returns (collections.defaultdict): Map of word counts.
+        """
         model = defaultdict(int)
-        for f in features:
-            model[f] += 1
+        for word in word_list:
+            model[word] += 1
         return model
 
-    def _edits1(self, word):
+    @staticmethod
+    def _single_edits(word):
+        """Set of single edits.
+
+        Given a word, return a set of edited versions that are one edit
+        away. These include single character deletions, transpositions,
+        replacements and inserts. Due to the likelihood of duplicates we
+        return a set.
+
+        :param (str) word: Word to generate single edits for.
+        :returns (set): The set of all single edits.
+        """
+        charset = string.ascii_lowercase + string.digits
         splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
         deletes = [a + b[1:] for a, b in splits if b]
         transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b) > 1]
-        replaces = [a + c + b[1:] for a, b in splits for c in self.charset if b]
-        inserts = [a + c + b for a, b in splits for c in self.charset]
+        replaces = [a + c + b[1:] for a, b in splits for c in charset if b]
+        inserts = [a + c + b for a, b in splits for c in charset]
         return set(deletes + transposes + replaces + inserts)
 
-    def _known_edits2(self, word):
-        return set(e2 for e1 in self._edits1(word) for e2 in self._edits1(e1)
-                   if e2 in self.nwords)
+    @staticmethod
+    def _double_edits(word):
+        """Set of double edits.
 
-    def _known_substr(self, words):
-        return set(w for w in words if w in self.nwords)
+        Using single edit misspellings returns a set of words that are two
+        edits away.
+
+        :param (str) word: Word to generate edits for.
+        :returns (set): The set of all double edits.
+        """
+        return set(e2 for e1 in PhraseLookup._single_edits(word)
+                   for e2 in PhraseLookup._single_edits(e1))
 
     def _known(self, words):
-        return set(w for w in words if w in self.nwords)
+        """Set of known words.
 
-    def correct_token(self, token):
-        candidates = (self._known([token]) or self._known(self._edits1(token)
-                      or self._known_edits2(token)))
-                       # self._known_substr(token)))
-        if candidates:
-            return max(candidates, key=self.nwords.get)
-        return 'None'
+        Given a set of words, returns the subset that are in the model, i.e.
+        known words.
 
-    def correct_phrase(self, text):
-        tokens = text.split()
-        return [self.correct_token(token) for token in tokens]
+        :param (set) words: A list of words.
+        :returns (set): The subset of words that are known to the model.
+        """
+        return set(w for w in words if w in self.model)
+
+    def _lookup_word(self, word):
+        """Lookup word.
+
+        Given a word, use the model to find the most likely candidate match.
+
+        :param (str) word: Word to look up.
+        :returns (str): Either a known word from the model determined from the
+          single or double edits else the supplied word.
+        """
+        candidates = (self._known({word}) or
+                      self._known(self._single_edits(word)) or
+                      self._known(self._double_edits(word)) or
+                      {word})
+        return max(candidates, key=self.model.get)
+
+    def lookup_phrase(self, phrase):
+        """Look up phrase.
+
+        Given a phrase, lookup each word in the phrase.
+
+        :param (str) phrase: Phrase to look up.
+        :returns (list): List containing each candidate match for word in
+          supplied phrase, or the original word in the supplied phrase if no
+          candidate was found.
+        """
+        words = phrase.split()
+        return [self._lookup_word(word) for word in words]
